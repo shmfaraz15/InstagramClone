9a697d9dcddf8c91d47ca7e4b7850b2517cb0746
shmfaraz15 <shm.faraz15@gmail.com>
2020-09-03 12:39:39 +0530

    Getting data from parse server

--- app/src/main/java/com/example/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -6,17 +6,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editTextName, editTextPunchSpeed, editTextPunchPower, editTextKickSpeed, editTextKickPower;
     private Button saveButton;
+    private TextView textViewGetData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         editTextKickPower = findViewById(R.id.edt_kickPower);
 
         saveButton = findViewById(R.id.btn_save);
+
+        textViewGetData=findViewById(R.id.txt_getData);
     }
 
     public void save(View view) {
@@ -61,5 +70,56 @@ public class MainActivity extends AppCompatActivity {
             FancyToast.makeText(this, e.getMessage(),
                     FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
         }
+    }
+
+    public void getData(View view) {
+
+//        ParseQuery<ParseObject> objectParseQuery=ParseQuery.getQuery("KickBoxer");
+//        objectParseQuery.getInBackground("OAqRZH3Cnc", new GetCallback<ParseObject>() {
+//            @Override
+//            public void done(ParseObject object, ParseException e) {
+//                if (object!=null && e==null){
+//                    String output="KickBoxer Name : "+object.get("name")+"\n"+
+//                            "Punch Speed : "+object.get("punchSpeed")+"\n"+
+//                            "Punch Power : "+object.get("punchPower")+"\n"+
+//                            "Kick Speed : "+object.get("kickSpeed")+"\n"+
+//                            "Kick Power : "+object.get("kickPower");
+//
+//                    textViewGetData.setText(output);
+//                }
+//                else{
+//                    FancyToast.makeText(MainActivity.this, e.getMessage(), FancyToast.LENGTH_LONG,
+//                            FancyToast.ERROR, true).show();
+//                }
+//            }
+//        });
+
+        ParseQuery<ParseObject> parseQuery=ParseQuery.getQuery("KickBoxer");
+        parseQuery.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                String output="";
+                if (e==null) {
+                    if (objects.size() > 0) {
+                        for (ParseObject object : objects) {
+                            output += "KickBoxer Name : " + object.get("name") + "\n" +
+                                    "Punch Speed : " + object.get("punchSpeed") + "\n" +
+                                    "Punch Power : " + object.get("punchPower") + "\n" +
+                                    "Kick Speed : " + object.get("kickSpeed") + "\n" +
+                                    "Kick Power : " + object.get("kickPower") + "\n\n";
+                        }
+
+                        textViewGetData.setText(output);
+                    } else {
+
+                        FancyToast.makeText(MainActivity.this, "list size is zero", FancyToast.LENGTH_LONG,
+                                FancyToast.ERROR, true).show();
+                    }
+                }else {
+                    FancyToast.makeText(MainActivity.this, e.getMessage(), FancyToast.LENGTH_LONG,
+                            FancyToast.ERROR, true).show();
+                }
+            }
+        });
     }
 }
