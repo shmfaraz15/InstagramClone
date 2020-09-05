a6585eeb8b8ef061d3ac71d0a44554a9c3f87f45
shmfaraz15 <shm.faraz15@gmail.com>
2020-09-05 12:07:07 +0530

    Instagram Login and SignUp Complete Activities

--- app/src/main/java/com/example/instagramclone/UsersTab.java
+++ app/src/main/java/com/example/instagramclone/UsersTab.java
@@ -7,58 +7,78 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UsersTab#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class UsersTab extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     public UsersTab() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UsersTab.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static UsersTab newInstance(String param1, String param2) {
         UsersTab fragment = new UsersTab();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_tab, container, false);
+        View view= inflater.inflate(R.layout.fragment_users_tab, container, false);
+
+        final ArrayList<String> arrayList=new ArrayList<>();
+        listView=view.findViewById(R.id.listView);
+
+        ParseQuery<ParseUser> parseQuery=ParseUser.getQuery();
+
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+                if (e==null){
+                    if (users.size()>0){
+
+                        for (ParseUser user:users){
+                            arrayList.add(user.getUsername());
+                        }
+
+                        arrayAdapter=new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,arrayList);
+                        listView.setAdapter(arrayAdapter);
+                    }else {
+                        FancyToast.makeText(getContext(), "List size is zero",
+                                FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+                    }
+                }else {
+                    FancyToast.makeText(getContext(), e.getMessage(),
+                            FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+                }
+            }
+        });
+        return view;
     }
 }
